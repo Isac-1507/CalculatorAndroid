e13261038de6113398c5ededf85e3a69f995467d
Isac-1507 <I.Evans2010@proton.me>
2024-09-28 22:04:48 +0000

    Add build code

---
+++ android/app/src/main/java/com/Isac_1507/Calculator/MainActivity.java
@@ -0,0 +1,5 @@
+package com.Isac_1507.Calculator;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Isac_1507.Calculator',
+  appName: 'Calculator',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
